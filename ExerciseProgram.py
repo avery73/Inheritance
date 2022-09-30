a60b71e6574177a2e235c82f24426b49add9612c
Avery <sherlock@holmes.com>
2022-09-30 14:59:18 -0500

    true

--- ExerciseProgram.py
+++ ExerciseProgram.py
@@ -6,7 +6,7 @@ def main():
     address = "1234 Main St"
     telephone = "123-456-7890"
     cust_num = 1234
-    on_list = False # has to be uppercase False
+    on_list = True # has to be uppercase False
 
     person1 = E.Person(name, address, telephone) # instance of superclass
     customer1 = E.Customer(name, address, telephone, cust_num, on_list) # instance of customer class
